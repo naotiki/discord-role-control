4e69de6199dab58be27b810b650d6b61644cbdd4
Naotiki <j22413@inc.kisarazu.ac.jp>
2022-11-01 23:15:45 +0900

    Chakra UIの初期設定

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,23 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import {ChakraProvider} from "@chakra-ui/react";
+import { extendTheme } from '@chakra-ui/react'
 
-export default function App({ Component, pageProps }: AppProps) {
+// 2. Extend the theme to include custom colors, fonts, etc
+const colors = {
+    brand: {
+        900: '#1a365d',
+        800: '#153e75',
+        700: '#2a69ac',
+    },
+}
+
+export const theme = extendTheme({ colors })
+function MyApp({ Component, pageProps }: AppProps) {
   return (
-      <ChakraProvider>
+      <ChakraProvider theme={theme}>
         <Component {...pageProps} />
       </ChakraProvider>
   )
 }
+export default MyApp
