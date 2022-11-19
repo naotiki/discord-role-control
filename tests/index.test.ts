fb9a8e51bdfe44faed27095c0029de6615d489ad
Naotiki <j22413@inc.kisarazu.ac.jp>
2022-11-19 14:15:28 +0900

    Test導入

---
+++ tests/index.test.ts
@@ -0,0 +1,9 @@
+import {DiscordSnowflake} from "@sapphire/snowflake";
+import {ObjectId} from "mongodb";
+
+describe('Home', function () {
+    it('should s', function () {
+        const s = DiscordSnowflake.deconstruct("684655306764058644");
+
+    });
+});
